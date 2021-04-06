ff9e66841bc761b107e7b84854e2752d812ae529
Adam-Henrie <adamhenriem@gmail.com>
2021-04-06 15:26:40 -0400

    19th Commit Sub From Funds and sub from inventory now working.

--- app/src/main/java/com/example/live_courier_ut3/DriverDetails.java
+++ app/src/main/java/com/example/live_courier_ut3/DriverDetails.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -26,6 +33,14 @@ public class DriverDetails extends AppCompatActivity {
     TextView driver;
     String store;
     String driverName;
+    ImageView star1;
+    ImageView star2;
+    ImageView star3;
+    ImageView star4;
+    ImageView star5;
+
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +53,28 @@ public class DriverDetails extends AppCompatActivity {
 
         grabStars = findViewById(R.id.grabStars);
         grabDistance = findViewById(R.id.grabDistance);
-        stars = findViewById(R.id.tv_grab_stars);
+
         distance = findViewById(R.id.tv_distance);
         pickThisDriver = findViewById(R.id.btn_pick_driver);
         driver = findViewById(R.id.driverProfile);
+        star1 = findViewById(R.id.star1);
+        star2 = findViewById(R.id.star2);
+        star3 = findViewById(R.id.star3);
+        star4 = findViewById(R.id.star4);
+        star5 = findViewById(R.id.star5);
+
+
+
+
+//        Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+//        Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star2);
+//        Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star3);
+//        Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star4);
+//        Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star5);
+
+       // star5.setVisibility(View.INVISIBLE);
+
+
 
         driver.setText(driverName);
 
@@ -53,17 +86,43 @@ public class DriverDetails extends AppCompatActivity {
                 docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
-                       String starRatings = documentSnapshot.getString("stars");
+                        String starRatings = documentSnapshot.getString("stars");
+                        int id = Integer.parseInt(starRatings);
 
-
-                       stars.setText(starRatings);
-
-
+                        switch (id) {
+                            case 1:
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+                                return;
+                            case 2:
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star2);
+                                return;
+                            case 3:
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star2);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star3);
+                                return;
+                            case 4:
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star2);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star3);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star4);
+                                return;
+                            case 5:
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star1);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star2);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star3);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star4);
+                                Picasso.get().load("http://images.wikia.com/glee/images/a/a0/Gold_Star.png").into(star5);
+                                return;
+                        }
                     }
                 });
-
             }
-        });
+            });
+
+
+
 
 
         grabDistance.setOnClickListener(new View.OnClickListener() {
@@ -87,6 +146,8 @@ public class DriverDetails extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(),ItemTracking.class);
                 intent.putExtra("store",store);
+
+
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/live_courier_ut3/PaymentPage.java
+++ app/src/main/java/com/example/live_courier_ut3/PaymentPage.java
@@ -2,6 +2,7 @@ package com.example.live_courier_ut3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +19,11 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-import java.sql.Driver;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class PaymentPage extends AppCompatActivity {
@@ -35,6 +39,23 @@ public class PaymentPage extends AppCompatActivity {
     String funds;
     Button addFunds;
     Button agreeAndPay;
+
+
+
+  public  DocumentReference cartItemLookup;
+
+  public  DocumentReference removeFromInventory;
+
+  public  DocumentReference mDocRef;
+
+  public  ArrayList<Map<String,String>> itemName;
+
+   public ArrayList<String> numInv;    //new ArrayList<String>(Collections.emptyList());
+
+
+   public List<String> mNames = new ArrayList<String>();
+    public List<Integer> mIndex = new ArrayList<Integer>();
+    public String numUpdate;
 
     String fundsKey = "funds";
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +85,7 @@ public class PaymentPage extends AppCompatActivity {
      //   DocumentReference mDocRef = FirebaseFirestore.getInstance().document("users/" + FirebaseAuth.getInstance().getCurrentUser() + "/userFunds/funds");
 
         //Adam Henrie changed
-        DocumentReference mDocRef = FirebaseFirestore.getInstance().document("users/" + FirebaseAuth.getInstance().getCurrentUser().getDisplayName() + "/userFunds/funds");
+         mDocRef = FirebaseFirestore.getInstance().document("users/" + FirebaseAuth.getInstance().getCurrentUser().getDisplayName() + "/userFunds/funds");
 
         mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
             @Override
@@ -121,6 +142,16 @@ public class PaymentPage extends AppCompatActivity {
             }
         });
 
+//thinking of grabbing the itemList from the cart. Iterating through list and progressively removing items as the program sees them.
+        //this code does not account for item overdraw where the user tries to buy more items than there are currently in the inventory.
+         cartItemLookup = FirebaseFirestore.getInstance().document("users/"
+                + FirebaseAuth.getInstance().getCurrentUser().getDisplayName()
+                + "/storeCart/"
+                 + store + "Cart"
+                );
+
+         removeFromInventory = FirebaseFirestore.getInstance().document("stores/" + store + "/itemInfo/items");
+
 
         agreeAndPay.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,7 +159,83 @@ public class PaymentPage extends AppCompatActivity {
                 Intent intent = new Intent(v.getContext(), DriversList.class);
                 //need to send the store location to the item tracking page to start the tracking bard on the item tracking page.
                 intent.putExtra("store", store);
-                startActivity(intent);
+
+                Map<String, Object> subFunds = new HashMap<>();
+                Double updatedFunds = Double.parseDouble(funds) - Double.parseDouble(storeTotal);
+                subFunds.put("fundsLeft", Double.toString(updatedFunds) );
+
+                mDocRef.update(subFunds);
+                Log.d("Payment Page: ", "order total has be subbed from funds");
+
+                //removing items from the inventory of the store before switching activities
+
+
+                cartItemLookup.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                         itemName = (ArrayList<Map<String, String>>) documentSnapshot.get("itemList");
+                        Log.d(TAG, "here is the itemList from the store" + Arrays.asList(itemName).toString());
+                        for (int i = 0; i < itemName.size(); i++) {
+                            HashMap<String, String> hashMap = new HashMap<String, String>();
+                            hashMap = (HashMap<String, String>) itemName.get(i);
+                            Iterator<String> mapItr = hashMap.keySet().iterator();
+                            while (mapItr.hasNext()) {
+                                String key = mapItr.next();
+                                mNames.add(key);
+                                mIndex.add(i);
+
+                            }
+                            Log.d(TAG, "onSuccess: mIndex " + mIndex);
+                        }
+
+
+                    }
+                });
+
+
+                removeFromInventory.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists()){
+                            numInv = (ArrayList<String>) documentSnapshot.get("itemNumber");
+                            assert numInv != null;
+                        //    Log.d(TAG, "Checking the value of numInv" + numInv.toString());
+                        } else {
+                            Log.d(TAG, "this is not working!");
+                        }
+
+                    }
+                });
+
+               // Log.d(TAG, "checking value of mNames " + mNames.toString());
+
+
+//                for(int y = 0; y < size; y ++){
+//                    Map<String, Object> updateInv = new HashMap<String,Object>();
+//                     Log.d(TAG, "Re-checking the value of numInv" + numInv.toString());
+//                    int indexOfName = numInv.indexOf(mNames.get(y));
+//                    Log.d(TAG, "checking for indexOfName " + indexOfName);
+//                    numUpdate = numInv.get(numInv.indexOf(mNames.get(y)));
+//                    numUpdate = String.valueOf(Integer.parseInt(numUpdate) - 1);
+//                    numInv.set(indexOfName,numUpdate);
+//
+//                    updateInv.put("itemNumber", numInv);
+//                    removeFromInventory.update(updateInv);
+//                    removeFromInventory.set(updateInv);
+//                }
+
+                Handler h = new Handler();
+
+                h.postDelayed(new Runnable() {
+                    public void run() {
+                        int size = mNames.size();
+
+                        forLoop(size, mNames, mIndex);
+                        startActivity(intent);
+                    }
+                }, 5000);
+
+
             }
         });
 
@@ -141,7 +248,29 @@ public class PaymentPage extends AppCompatActivity {
 
 
 
+    void forLoop(int sizze, List<String> mNamess, List<Integer> index){
+        int size = sizze;
+        List<String> mNammmessss = mNamess;
+        List<Integer> mIndexx = index;
+        Log.d(TAG, "is this running!!!!!");
+        for(int y = 0; y < size; y ++){
+                    Map<String, Object> updateInv = new HashMap<String,Object>();
+                     Log.d(TAG, "Re-checking the value of numInv" + numInv.toString());
+                     Log.d(TAG, "names passed to method? " + mNammmessss);
+                     Log.d(TAG, "first name from names " + mNammmessss.get(y));
+                     Log.d(TAG, "index array " + mIndexx);
+                    int indexOfName = mIndexx.get(y);
+                    Log.d(TAG, "checking for indexOfName " + indexOfName);
+                    numUpdate = numInv.get(indexOfName);
+                    numUpdate = String.valueOf(Integer.parseInt(numUpdate) - 1);
+                    numInv.set(indexOfName,numUpdate);
 
+                    updateInv.put("itemNumber", numInv);
+                    removeFromInventory.update(updateInv);
+                    //this commented out command underneath can be very evil if you call ".set" it sets only the selected data field and wipes the reset from firestore
+                  //  removeFromInventory.set(updateInv);
+                }
+    }
 
 
 }
